3baeee54d16851ee620bbb5ca6e3a667cd2492de
Muhamad Rafli Shidiq <muhamadraflishidiq@gmail.com>
2026-08-19 11:33:31 +0700

    feat: add component footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <div className="p-10 flex flex-row gap-10 justify-between bg-[#F8FAFC] text-base text-gray-500 font-bold">
+      <span>&copy; 2024 SHORTLINK. THE DIGITAL ARHICTECT.</span>
+      <span>PRIVACY POLICY</span>
+      <span>TERMS OF SERVICE</span>
+      <span>API DOCUMENTATION</span>
+      <span>SUPPORT</span>
+    </div>
+  );
+};
+
+export default Footer;
