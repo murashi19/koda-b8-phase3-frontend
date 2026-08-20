f5a71ff752c8efc36da452182358943bd5c70900
Muhamad Rafli Shidiq <muhamadraflishidiq@gmail.com>
2026-08-20 11:46:27 +0700

    fix: fixing bug

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:8080",
-  baseURL: import.meta.env.BACKEND_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use(
